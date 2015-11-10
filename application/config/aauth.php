352829f23de23a519b1e32cbee76e88ad640cd91
Bitblade <github@bestpc.org>
2015-11-10 17:25:42 +0100

    Added comments to the configuration file

--- application/config/aauth.php
+++ application/config/aauth.php
@@ -64,8 +64,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | 	['verification_link']              	Link for verification without site_url or base_url
 | 	['reset_password_link']            	Link for reset_password without site_url or base_url
 |
-|	['hash']							Name of selected hashing algorithm (e.g. "md5", "sha256", "haval160,4", etc..)
-|										Please, run hash_algos() for know your all supported algorithms
+|	['use_php_password_hash']			Use the new password_hash function in PHP 5.5+ for added security			
+|
+|	['hash']							Using password_hash:
+|											Constant of the hashing algorithm used to hash new/updated passwords.
+|											See: http://php.net/manual/en/function.password-hash.php 
+|										NOT using password_hash:
+|											Name of selected hashing algorithm (e.g. "md5", "sha256", "haval160,4", etc..)
+|											Please, run hash_algos() for know your all supported algorithms.
+|										
+|										
 |
 */
 $config_aauth = array();
